8aca40304cb495d54d37bfd65ee1ce2fc85d04f2
Matheus Ramos <89211913+Matheus0820@users.noreply.github.com>
2026-03-19 01:04:46 +0000

    feat: Exercícios da aula 5

--- Aula5/exercicio01.js
+++ Aula5/exercicio01.js
@@ -1,3 +1,53 @@
 // Simular a busca de um usuário em um banco de dados de modo assincrono
 // 1 - Criar uma função chamada buscarUsuario(id, callback) -> O callback deve receber o erro como primeiro parametro e os dados como segundo parametro
 // 2 - Fazer um código para testar essa função
+
+
+// Função
+function buscarUsuario(id, callback) {
+    console.log(`Buscando usuário com id = ${id}...`)
+
+    setTimeout(() => {
+        if (id < 0) {
+            err_mensage = `ID inválido - Usuário não existe com o id = ${id}`
+            callback(err_mensage, null);
+            return;
+        }
+
+        // Criado um usuário - simulando consulta no banco
+        const userConsulte = {
+            id: id,
+            name: "nameTeste",
+            year: 20
+        }
+
+        callback(null, userConsulte)
+
+    }, 2000)
+
+}
+
+/* Testando Função */
+
+// Dar certo -> err = null
+buscarUsuario(-1, (err, data) => {
+    if(err) {
+        console.error(err)
+    }
+    else {
+        console.log("Consulta concluida - usuário encontrado")
+        console.info(`O usuário que possui o id: ${data.id}, tem o nome de: ${data.name}`)
+    }
+})
+
+// Dar errado - err != null
+buscarUsuario(10, (err, data) => {
+    if(err) {
+        console.log(err)
+    }
+    else {
+        console.log("Consulta concluida - usuário encontrado")
+        console.log(`O usuário que possui o id: ${data.id}, tem o nome de: ${data.name}`)
+    }
+})
+
--- Aula5/exercicio02.js
+++ Aula5/exercicio02.js
@@ -1,3 +1,53 @@
 // Criar 3 funções -> fazerPedido, prepararLanche e entregarLanche
 // Essas funções devem ser encadeadas com o callback -> Callback deve seguir o padrão de first-err -> callback = (err, data) => {code...}
 // Fazer um código de teste esse encadeamento de função -> Simulação do Callback Hell
+
+function fazerPedido(pedido, func_callback1) {
+    console.log("1. Fazendo pedido")
+    setTimeout(() => {
+        console.log(`Pedido de ${pedido} - Feito`)
+        func_callback1(null, pedido)
+
+    }, 2000);
+}
+
+function prepararLanche(pedido, func_callback2) {
+    setTimeout(() => {
+        console.log(`2. Preparando ${pedido}`)
+        func_callback2(null, `Lanche - "${pedido}" - feito`)
+    }, 4000)
+}
+
+function entregarLanche(pedido, func_callback3) {
+    setTimeout(() => {
+        console.log(`3. Saindo para entrega`)
+        func_callback3(null, "Pedido entregue")
+    }, 2000)
+}
+
+
+/* Código de teste */
+fazerPedido("X-bacon", (err, data) => {
+    if (err) {
+        console.error(err)
+    }
+    else {
+        console.log(`- ${data}`)
+        prepararLanche(data, (err1, data1) => {
+            if(err1) {
+                console.error(err1)
+            }
+            else {
+                console.log(`- ${data1}`)
+                entregarLanche(data1, (err2, data2) => {
+                    if (err2) {
+                        console.error(err2)
+                    }
+                    else {
+                        console.log(`- ${data2}`)
+                    }
+                })
+            }
+        })
+    }
+})
